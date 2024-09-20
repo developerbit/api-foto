bcc11107f116b47918a8de675ec1d40781a3dc23
julidavi777 <julian.david897@gmail.com>
2024-09-20 16:27:11 -0500

    se implementa el envio de errores en el env_service

--- load/services/env_service.py
+++ load/services/env_service.py
@@ -1,12 +1,13 @@
 import os
 from dotenv import load_dotenv
-
+from  utils.send_errors import send_errors    
 # Inicializar Sentry con tu DSN (Data Source Name)
 
 
 load_dotenv()
 
 def get_enviroment(name_env):
+
     """
     Buscador de variables.
     
@@ -14,16 +15,15 @@ def get_enviroment(name_env):
         name_env: nombre de la variable de entorno
     """
     try:
+        
         # Obtener la URL del endpoint de la API desde las variables de entorno
         env = os.getenv(name_env)
-        if env is None:
-            print(env, flush=True)
+        if env != None:
+            return env  
+        else:
             # Levanta un error si no se encuentra la variable de entorno
             raise KeyError(f"La variable de entorno {name_env} no existe o es inválida.")
-        else:
-            return env
         
     except KeyError as e:
-        # Captura la excepción con Sentry
-        return f'la variable de entorno {name_env} no fue hallada o no existe'
+        return send_errors(f'la variable de entorno {name_env} no fue hallada o no existe',500, 'env_service', 9)
 
