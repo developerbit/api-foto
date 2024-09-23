d3056c1b101d17d28ed984977d29e5d557f93986
julidavi777 <julian.david897@gmail.com>
2024-09-23 16:09:56 -0500

    veredicto_service ahora registra los errores correctamente en caso de ocurrir

---
+++ load/services/yolo_nas_services/veredicto_service.py
@@ -0,0 +1,35 @@
+from  utils.send_errors import send_errors    
+
+class VeredictoService:
+
+    def jsonVeredict(detalle_prediccion,imageId,id_modelo,etiquetas,imageUrl,telefono):
+        try:
+            for registro in detalle_prediccion:
+                veredictos = []
+                veredicto = {
+                    "imageId": imageId,
+                    "classname": registro["classname"],
+                    "yMin": registro["yMin"],
+                    "xMin": registro["xMin"],
+                    "yMax": registro["yMax"],
+                    "xMax": registro["xMax"],
+                }
+                veredictos.append(veredicto)
+
+            #crear la lôgica para el veredicto basado en etiquetas 
+            logica = [{
+                "id_modelo": id_modelo,
+                "etiquetas": etiquetas
+            }]
+            
+            #   crear el diccionario de resultados
+            resultados = {
+                "imageId": imageId,
+                "url_image": imageUrl,
+                "telefono": telefono,
+                "veredictos": veredictos,
+                "logica": logica,
+            }
+            return veredictos,  resultados
+        except KeyError as e:
+            return send_errors(str(e),500, 'prediction_service', 18)
